7c4f671c8756b8e6c348aa09765ff0f1f95b2566
NickLeylandMedia <nick@nick-leyland.com>
2021-02-28 00:38:19 -0500

    Added/styled/js coded multiple modules for kitchen sink.

--- JS Kitchen Sink/js/main.js
+++ JS Kitchen Sink/js/main.js
@@ -1,11 +1,14 @@
+//Change Color of Header with Button
 function changeHeaderColor() {
     document.getElementById('changeTextColorHeader').style.color = 'red';
 }
 
+//Change Image with Button
 function changeImageBasic() {
     document.getElementById('changingImage').src = './img/cheesy.png';
 }
 
+//Basic Slideshow
 function imageSlideshow() {
     var i = 0;
     var images = [];
@@ -43,13 +46,90 @@ function imageSlideshow() {
     document.getElementById('slideshowForward').addEventListener("click", nextImage);
 }
 
-function pushValueToArrayBasic() {
-    var basicArray = [];
+//Display Array Basic (Change with Buttons)
+function displayArrayBasic() {
+    disparray1 = ["Burger King", "McDonalds", "Wendys"];
+    document.getElementById('basicArrayText').innerHTML = disparray1;
+}
+
+function changeDisplayArrayBasicFWD() {
+    disparray2 = "Hamburgers and cheeseburgers work well with fries."
+    document.getElementById('basicArrayText').innerHTML = disparray2;
+}
+
+function changeDisplayArrayBasicBCK() {
+    disparray3 = ["Chilis", "Red Robin", "Ruby Tuesday"]
+    document.getElementById('basicArrayText').innerHTML = disparray3;
+}
+   
+//Push Value to Array
+var basicArray = [];
     basicArray[0] = 1;
     basicArray[1] = 2;
 
+function renderPushArray() {
     document.getElementById('buttonPushesToArrayDisplay').innerHTML = basicArray;
+}    
+function pushValueToArrayBasic() {
+    basicArray.push(3);
+    renderPushArray();
 }
 
+//Push Value to Array (INCREMENTING)
+var basicArrayLoop = [];
+    basicArrayLoop[0] = 1;
+    basicArrayLoop[1] = 2;
+
+var pushValue = 3;
+
+function renderPushArrayLoop() {
+    document.getElementById('buttonPushesToArrayLoopDisplay').innerHTML = basicArrayLoop;
+}    
+function pushValueToArrayLoopBased() {
+    basicArrayLoop.push(pushValue);
+    pushValue = pushValue + 1;
+    renderPushArrayLoop();
+}
+
+//Operate Value with Buttons
+var manipValue = 0;
+function valueRender() {
+    document.getElementById('buttonValue').innerHTML = manipValue;
+}
+
+function minusOneHundred() {
+    manipValue = manipValue - 100;
+    valueRender();    
+}
+
+function minusTen() {
+    manipValue = manipValue - 10;
+    valueRender();
+}
+
+function minusOne() {
+    manipValue = manipValue - 1;
+    valueRender();    
+}
+
+function plusOne() {
+    manipValue = manipValue + 1;
+    valueRender();    
+}
+
+function plusTen() {
+    manipValue = manipValue + 10;
+    valueRender();    
+}
+
+function plusOneHundred() {
+    manipValue = manipValue + 100;
+    valueRender();    
+}
+
+//Functions triggered on window load
 window.onload = imageSlideshow();
-window.onload = pushValueToArrayBasic();
+window.onload = renderPushArray();
+window.onload = displayArrayBasic();
+window.onload = valueRender();
+window.onload = renderPushArrayLoop();
