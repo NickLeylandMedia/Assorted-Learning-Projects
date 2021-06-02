ed49e171164fd2c8eed5df00a3abf2e6720dd5a0
NickLeylandMedia <nick@nick-leyland.com>
2021-06-02 18:44:38 -0400

    Expanded strings html/js, added arrays html/js.

--- Kitchen Sink Rebuild/js/strings.js
+++ Kitchen Sink Rebuild/js/strings.js
@@ -2,12 +2,10 @@ let addOne
 let addTwo
 let str
 
-let  guestList = `Guests:
-*Steven
-*John
-*Lily
-`
+let searchStr = "Six green slices of ham with the caviar for the bride of the honorable guest."
+let substr = "the"
 
+let indices = []
 
 /* Grab Values from Inputs and Display String*/
 //Add Values Together
@@ -38,5 +36,75 @@ let grabString = () => {
 
 let grabAndLeng = () => {
     grabString()
+    document.getElementById('stringDisp').innerHTML = str
     return document.getElementById('lengDisp').innerHTML = str.length
+}
+
+let strPos = (pos) => {
+    grabString()
+    document.getElementById('stringDisp').innerHTML = str
+    return document.getElementById('posDisp').innerHTML = str[pos]
+}
+
+//Loop over string and display characters
+
+//Change String to Uppercase
+let changeUpper = () => {
+    grabString()
+    document.getElementById('stringDisp').innerHTML = str
+    return document.getElementById('caseDisp').innerHTML = str.toUpperCase()
+}
+
+//Change String to Lowercase
+let changeLower = () => {
+    grabString()
+    document.getElementById('stringDisp').innerHTML = str
+    return document.getElementById('caseDisp').innerHTML = str.toLowerCase()
+}
+
+/* Search String for Value */
+//Render String
+let renderSearch = () => {
+    document.getElementById('searchDisp').innerHTML = searchStr
+}
+
+//Search for First Occurrence
+let search = (val) => {
+    renderSearch()
+    document.getElementById('resultDisp').innerHTML = searchStr.indexOf(val, 0)
+}
+
+//Search for All Occurrences
+function findAll(string, value) {
+    let currentIndex = string.indexOf(value)
+    while (currentIndex != -1) {
+        indices.push(currentIndex)
+        currentIndex = string.indexOf(value, currentIndex + value.length)
+    }
+    document.getElementById('resultDisp').innerHTML = indices
+    document.getElementById('totalDisp').innerHTML = indices.length
+    return indices;
+}
+
+//Check if String Includes
+let checkInclude = (value) => {
+    document.getElementById('inclDisp').innerHTML = "String includes " + value
+    document.getElementById('inclRes').innerHTML = searchStr.includes(value)
+}
+
+//Check if String Starts With
+let checkStart = (value) => {
+    document.getElementById('inclDisp').innerHTML = "String starts with " + value
+    document.getElementById('inclRes').innerHTML = searchStr.startsWith(value)
+}
+
+//Check if String Ends With
+let checkEnd = (value) => {
+    document.getElementById('inclDisp').innerHTML = "String ends with " + value
+    document.getElementById('inclRes').innerHTML = searchStr.endsWith(value)
+}
+
+let sliceFrom = (pos) => {
+    renderSearch()
+    return document.getElementById('sliceDisp').innerHTML = searchStr.slice(pos)
 }
