508dd233eb5667335461590a0ce2cc230ea7b997
NickLeylandMedia <nick@nick-leyland.com>
2021-06-12 10:25:08 -0400

    Expanded functions HTML/js with recursion.

--- Kitchen Sink Rebuild/js/functions.js
+++ Kitchen Sink Rebuild/js/functions.js
@@ -7,4 +7,32 @@ let askName = () => {
 
 let greeting = () => console.log('Hello ' + userName + '!');
 
+//Recursion
+function countDown(n) {
+    for (let i=n; i > 0; i--) {
+        console.log(i)
+    }
+}
+
+function reCountDown(n) {
+    console.log(n) 
+    if (n >= 1) reCountDown(n-1)
+}
+
+function reCountUp(n) {
+    console.log(n)
+    if (n <= 100)  reCountUp(n+1)
+}
+
+function pow(base, exp) {
+    if (exp == 1) {
+        return base
+    } else {
+        return base * pow(base, exp - 1)
+    }
+}
+
+function shortPow(base, exp) {
+    return (exp == 1) ? base : (base * shortPow(base, exp - 1))
+}
 
